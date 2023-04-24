f4d568df48044e847c5c602f00846ccdda93db0b
obulman <119469903+obulman@users.noreply.github.com>
2023-04-24 10:44:24 -0400

    Necessary script for about functionality
    For the tabs and accordions.

---
+++ scripts/about.js
@@ -0,0 +1,45 @@
+document.getElementById("defaultOpen").click();
+
+// This is a modified JS code of the W3 tab code
+
+function selectSkill(evt, skilltype) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(skilltype).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  // This is a modified Js code of the W3 accordion code
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
